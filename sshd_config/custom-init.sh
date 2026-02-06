5b7cf16dee8be41e2c5c2fcb6d2c484aac1d4f9c
Andrey Bash <andrej89536995626@gmail.com>
2026-02-06 22:59:53 +0300

    refactor: simplify docker-compose configuration
    - Add sshd_config to .gitignore
    - Remove volume mounts from docker-compose.yml
    - Remove custom-init.sh script

--- sshd_config/custom-init.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Custom SSHD config for verbose logging
-echo 'LogLevel VERBOSE' >> /config/sshd/sshd_config
-echo 'SyslogFacility AUTH' >> /config/sshd/sshd_config
-echo 'PrintLastLog yes' >> /config/sshd/sshd_config
-echo "[custom-init] SSH verbose logging enabled"
